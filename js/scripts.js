6824c147c60dc08fcc5b6f74ff0737e052236068
Aaron Whang <tlbhibiki@gmail.com>
2019-06-21 15:39:12 -0700

    it will add strings into the pizza object in order to print out the pizza attributes later. price function will ignore strings in calculations.

--- js/scripts.js
+++ js/scripts.js
@@ -11,14 +11,25 @@
 //make a pizza constructor
 //populate a new pizza object with toppings and tally the total
 //make size and type objects and add it to the pizza and output total
+//take in name and phone for new order
+//take in pizza and accumulate pizza price in pizza order objects
+//
 
 
+function PizzaOrder(name,phone,pizza,price) {
+  this.name = name;
+  this.phone = phone;
+  this.pizza = pizza;
+  this.price = price;
+}
 
-
-function Pizza(size,top,type) {
+function Pizza(size,sizeStr,top,topStr,type,typeStr) {
   this.size = size;
+  this.sizeStr = sizeStr;
   this.top = top;
+  this.topStr = topStr;
   this.type = type;
+  this.typeStr = typeStr;
 }
 
 Pizza.prototype.price = function() {
@@ -30,14 +41,14 @@ var sizes = {
   medium:    8,
   large:     10,
   xlarge:    12,
-}
+}                               // size object
 
 var types = {
   normal:     0,
   thin:       1,
   deep:       3,
   stuffed:    2,
-}
+}                               // types object
 
 var toppings = {
   cheese:     1,
@@ -52,20 +63,27 @@ var toppings = {
   pineapple:  .5,
   spinach:    .5,
   onion:      .5
-}
+}                            // topping object
 
-var meatlovers = new Pizza(0,[],0);
+var meatlovers = new Pizza(0,"",[],[],0,"");
+var pOrder = new PizzaOrder("","",[],[]);
 
-function sum( obj ) {                                                   //adds up the values inside an object, checks for arrays and adds up those values as well
+function sum( obj ) {                                  //calculates price of pizza                 //adds up the values inside an object, checks for arrays and adds up those values as well
   var sum = 0;
   for( var el in obj ) {
     if( obj.hasOwnProperty( el ) ) {
       if (Array.isArray(obj[el])) {
         for (var i = 0; i < obj[el].length; i++) {
+          if(isNaN(obj[el])) {
+          } else {
           sum += obj[el][i];
         }
+        }
       } else {
+        if(isNaN(obj[el])) {
+        } else {
         sum += parseFloat( obj[el] );
+      }
       }
     }
   }
@@ -73,26 +91,34 @@ function sum( obj ) {                                                   //adds u
 }
 
 
+function pizzaWrite(pizzaObject){             //takes in name of pizza object, reads form and calculates price, clears topping in pizza object
+  var pizzaSize = $('input[name=Size]:checked').val();
+  var pizzaType = $('input[name=pizzaType]:checked').val();
+  pizzaObject.size = sizes[pizzaSize];
+  pizzaObject.type = types[pizzaType];
+  pizzaObject.sizeStr = pizzaSize;
+  pizzaObject.typeStr = pizzaType;
+
+  $("input:checkbox[name=Topping]:checked").each(function(){
+    var toppingChecked = $(this).val();
+    pizzaObject.top.push(toppings[toppingChecked]);
+    pizzaObject.topStr = toppingChecked;
+  });
+
+  console.log(pizzaObject.sizeStr);
+  console.log(pizzaObject.typeStr);
+  console.log(pizzaObject.price());           //output pizza price
+  pizzaObject.top = [];
+}
 
 
 $(document).ready(function(){
 
   $("form#make_pizza").submit(function(event){
     event.preventDefault();
-    var pizzaSize = $('input[name=Size]:checked').val();
-    var pizzaType = $('input[name=pizzaType]:checked').val();
-    console.log(pizzaSize);
-    console.log(pizzaType);
-    meatlovers.size = sizes[pizzaSize];
-    meatlovers.type = types[pizzaType];
+    pizzaWrite(meatlovers);
 
-    $("input:checkbox[name=Topping]:checked").each(function(){
-      var toppingChecked = $(this).val();
-      meatlovers.top.push(toppings[toppingChecked]);
-    });
 
-    console.log(meatlovers.price());
-    meatlovers.top = [];
   });
 
 
